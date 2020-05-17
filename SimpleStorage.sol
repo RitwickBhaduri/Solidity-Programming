e519627a7ddebdd55ba2f0b55d83db7841457379
Ritwick_Bhaduri <54306152+RitwickBhaduri@users.noreply.github.com>
2020-05-18 03:17:39 +0530

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,28 +1,23 @@
-pragma solidity ^0.5.9; 
-// Imagine a big integer that the whole world could share
+pragma solidity ^0.4.0;
+
 contract SimpleStorage {
-    uint storedData;
-
-    function set(uint x) public {
-        storedData = x;
-    }
-
-    function get() view public returns (uint) {
-        return storedData;
-    }
-    
-    function increment (uint n) public {
-        storedData = storedData + n;
-        return;
-    }
-    
-    function decrement (uint n) public {
-        storedData = storedData - n;
-        return;
-    }
-    
-}
-
+ uint storedData;
  
-
-
+ function set(uint x) public {
+     storeData = x;
+ }
+ 
+ function get() constant public returns (uint) {
+     return storedData;
+ }
+ 
+ function increment(uint n) public {
+     storedData = storedData + n;
+     return;
+ }
+ 
+ function decrement(uint n) public {
+     storedData = storedData - n;
+     return;
+ }
+}
